c9185358d5377a5f6631b2a98b403bd9a86342d3
ROLERKUPS <rolerkups@gmail.com>
2025-09-06 20:59:37 +0930

    v0.2.2

--- main.py
+++ main.py
@@ -1,22 +1,130 @@
-# LAST UPDATED 2/09/2025 11:41 PM
+# LAST UPDATED 6/09/2025 8:57 PM
 
 import random
 import os
 import time
+import threading
+
+start_time = time.time()
+days_passed = 0
+day_length = 180
+ATK = 0
+knife = False
+poo = False
+shark_tracking = -1
 
 cash = 500
 ring = True
 loan = True
-slot_rules_shown = False  # NEW: flag to track if rules have been shown
+slot_rules_shown = False 
+
+def track_days():
+    global days_passed
+    global shark_tracking
+
+    while True:
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+        if elapsed_time >= (days_passed + 1) * 180:
+            days_passed += 1
+            os.system("cls")
+            print("You decide it's too late and time to call it a night.")
+            print(f"DAY: {days_passed}")
+            time.sleep(3)
+            if poo and days_passed >= shark_tracking + 5:
+                    loan_shark_event()
+                    shark_tracking = days_passed
+                    return_to_game()
+        time.sleep(1)
+
+def loan_shark_event():
+    global shark_tracking, knife, poo, cash, ring, loan, ATK
+    while True:
+        os.system("cls")
+        print("Five days have passed, and as you leave the casino the loan shark drags you into a nearby alley, knife in hand.")
+        time.sleep(4)
+        print("The man furiously demands the payment of $1000 you owe him")
+        time.sleep(3)
+        if cash > 1000:
+            print("What do you decide to?\n")
+            print("1. Pay him the $1000")
+            print("2. Refuse to pay him (fight)")
+            print("3. Attempt to negotiate paying him more later")
+            loan_shark_selection = int(input("SELECT EITHER '1', '2', OR '3': "))
+            if loan_shark_selection == 1:
+                os.system("cls")
+                cash -= 1000
+                print("You pay the man, and he leaves in a hurry.")
+                return_to_game()
+                return
+            elif loan_shark_selection == 2:
+                os.system("cls")
+                fight_chance = random.randint(1,100)
+                if fight_chance <= 10 + ATK:
+                    print ("You managed to knock him out. You take his stuff.\n")
+                    print("CASH: +1000")
+                    cash += 1000
+                    print("ITEM: +KNIFE")
+                    knife = True
+                    return_to_game()
+                    return
+            elif loan_shark_selection == 3:
+                os.system("cls")
+                print("You try to run, but the man is blocking the exit.")
+                time.sleep(2)
+                return
+        else:
+            print("You don't have enough money to pay the man back.")
+            time.sleep(2)
+            print("What do you decide to do?\n")
+            print("1. Attempt to negotiate a deal")
+            print("2. Refuse to pay him (fight)")
+            loan_shark_selection2 = int(input("SELECT EITHER '1', OR '2': "))
+            if loan_shark_selection2 == 1:
+                negotiate_chance = random.randint(1,100)
+                if negotiate_chance <= 50:
+                    print("The loan shark beats you badly, and says you have one more day to pay $2000")
+                    time.sleep(2)
+                    return_to_game()
+                    return
+                else:
+                    print("The loan shark stabs you, and everything goes dark.")
+                    exit()
+            elif loan_shark_selection2 == 2:
+                os.system("cls")
+                fight_chance = random.randint(1,100)
+                if fight_chance <= 10 + ATK:
+                    print ("You managed to knock him out. You take his stuff.\n")
+                    print("CASH: +1000")
+                    cash += 1000
+                    print("ITEM: +KNIFE")
+                    knife = True
+                    return_to_game()
+                    return
+                else:
+                    print("The loan shark stabs you, and everything goes dark.")
+                    exit()
+     
+def start_day_tracking():
+    import threading
+    thread = threading.Thread(target=track_days)
+    thread.daemon = True
+    thread.start()
+
+start_day_tracking()
+
+def return_to_game():
+    menu()
 
 def menu():
+    global days_passed
     
     if cash <1:
         broke()
 
     while True:
         os.system("cls")
-        print("Welcome to the casino! | v0.2.1a")
+        print("Welcome to the casino! | v0.2.2")
         print("by: ROLERKUPS\n")
         print("1 - Slots\n")
         print("2 - Roulette\n")
@@ -40,9 +148,7 @@ def menu():
         
 def broke():
 
-    global ring
-    global cash
-    global loan
+    global ring, cash, loan, poo, shark_tracking
 
     if ring == True:
         os.system("cls")
@@ -56,6 +162,7 @@ def broke():
         cash = 450
         return
     elif loan == True:
+        shark_tracking = days_passed
         os.system("cls")
         print("With nowhere left to turn, you set up a meet with one of the city's cruelest loan sharks.")
         time.sleep(3)
@@ -63,6 +170,7 @@ def broke():
         time.sleep(3)
         print("You head straight back to the casino.")
         time.sleep(3)
+        poo = True
         loan = False
         cash = 500
         return
